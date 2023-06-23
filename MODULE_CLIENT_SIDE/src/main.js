89ae9efb57d3443a0af34baf33b1ab880f9c3cbe
Marvvllo <marvtfs@gmail.com>
2023-06-23 13:56:42 +0800

    Added zombie spawning logic

--- MODULE_CLIENT_SIDE/src/main.js
+++ MODULE_CLIENT_SIDE/src/main.js
@@ -4,6 +4,8 @@ const k = kaboom()
 
 k.loadSprite("bean", "sprites/bean.png")
 k.loadSprite("bg", "sprites/General/Background.jpg")
+k.loadSprite("lawnMowerIdle", "sprites/General/lawnmowerIdle.gif")
+k.loadSprite("lawnMowerActive", "sprites/General/lawnmowerActivated.gif")
 
 loadSpriteAtlas("sprites/Zombie/spritesheet.png", {
 	"zombie": {
@@ -34,7 +36,7 @@ loadSpriteAtlas("sprites/PeaShooter/spritesheet.png", {
 const GAME_WIDTH = 1024
 const GAME_HEIGHT = 1024
 const LANE_WIDTH = 650
-const LANE_HEIGHT = 125
+const LANE_HEIGHT = 80
 const MARGIN = 16
 const PLANT_SLOTS = 8
 
@@ -47,35 +49,86 @@ add([
 	z(-2)
 ])
 
+// Lanes
 const lane1 = add([
 	'lane1',
 	pos(100, 125),
 ])
 
-const lane1Zombie = add([
-	'zombie',
-	health(5),
-	pos(LANE_WIDTH, lane1.pos.y - 20),
-	// scale(.75),
-	sprite("zombie"),
-	move(LEFT, 12),
-	area(),
-	z(1)
+const lane2 = add([
+	'lane1',
+	pos(100, 140 + LANE_HEIGHT),
 ])
 
-// triggers when hp reaches 0
-lane1Zombie.on("death", () => {
-	destroy(lane1Zombie)
-})
-
-const lane1PeaShooter = add([
-	'peaShooter',
-	pos(lane1.pos.x, lane1.pos.y),
-	scale(1.1),
-	sprite("pea"),
-	area(),
-	z(2)
+const lane3 = add([
+	'lane1',
+	pos(100, 145 + (2 * LANE_HEIGHT)),
 ])
+
+const lane4 = add([
+	'lane1',
+	pos(100, 150 + (3 * LANE_HEIGHT)),
+])
+
+const lane5 = add([
+	'lane1',
+	pos(100, 150 + (4 * LANE_HEIGHT)),
+])
+
+// Lawn Mower
+add([
+	pos(lane1.pos.x - 100, lane1.pos.y),
+	sprite('lawnMowerIdle')
+])
+
+const spawnLawnMower = () => {
+
+}
+
+// Zombie
+const spawnZombie = (lane) => {
+	const zombie = add([
+		'zombie',
+		health(5),
+		pos(LANE_WIDTH, lane.pos.y - 20),
+		sprite("zombie"),
+		move(LEFT, 12),
+		area(),
+		z(1),
+		offscreen({ destroy: true }),
+	])
+	// triggers when hp reaches 0
+	zombie.on("death", () => {
+		destroy(zombie)
+	})
+
+	loop(4, () => {
+		zombie.play("walk")
+	})
+}
+
+
+// Pea Shooter
+const spawnPeaShooter = (lane) => {
+	const peaShooter = add([
+		'peaShooter',
+		pos(lane.pos.x, lane.pos.y),
+		scale(1.1),
+		sprite("pea"),
+		area(),
+		z(2),
+		offscreen({ destroy: true }),
+	])
+
+	loop(2, () => {
+		peaShooter.play("idle")
+	})
+
+	// Shoot Pea loop
+	loop(2, () => {
+		shootPea(peaShooter)
+	})
+}
 
 const shootPea = (peaShooter) => {
 	add([
@@ -90,17 +143,48 @@ const shootPea = (peaShooter) => {
 	])
 }
 
-loop(1, () => {
-	shootPea(lane1PeaShooter)
-})
-
-loop(4, () => {
-	lane1PeaShooter.play("idle")
-	lane1Zombie.play("walk")
-})
-
 onCollideUpdate('zombie', 'pea', (zombie, pea) => {
 	zombie.hurt(1)
 	destroy(pea)
 	console.log("hit")
 })
+
+// Game loop
+wait(3, () => {
+
+	loop(5, () => {
+		const randomLane =
+			Math.floor(Math.random() * 5) + 1;
+
+		let lane = lane1
+
+		switch (randomLane) {
+			case 1: {
+				lane = lane1
+				break;
+			}
+			case 2: {
+				lane = lane2
+				break;
+			}
+			case 3: {
+				lane = lane3
+				break;
+			}
+			case 4: {
+				lane = lane4
+				break;
+			}
+			case 5: {
+				lane = lane5
+				break;
+			}
+		}
+
+		spawnZombie(lane)
+	})
+
+})
+
+
+spawnPeaShooter(lane1)
