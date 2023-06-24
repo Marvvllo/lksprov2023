5fbc6e4d045385425e3caab1349220bdacbd743d
Marvvllo <marvtfs@gmail.com>
2023-06-24 10:19:14 +0800

    Added assets

--- module-frontend/src/pages/_app.js
+++ module-frontend/src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/assets/css/bootstrap.css" />
+        <link rel="stylesheet" href="/assets/css/custom.css" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
